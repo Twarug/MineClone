9db7bd0229f8412bfef7fc242e49d5e510efe651
twarug <patryk280502@gmail.com>
2022-11-11 13:35:43 +0100

    Init

---
+++ MineClone/src/main.cpp
@@ -0,0 +1,8 @@
+#include "mcpch.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello, world!!!\n";
+    
+    return 0;
+}
---
+++ Premake5.lua
@@ -0,0 +1,71 @@
+workspace("MineClone")
+    architecture("x86_64")
+    startproject("MineClone")
+
+    configurations({
+        "Debug",
+        "Release",
+        "Dist"
+    })
+
+    flags({
+        "MultiProcessorCompile"
+    })
+    
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+    
+    group("Vendors")
+    -- include("MineClone/vendor")
+    group ("")
+
+project("MineClone")
+    location ("MineClone")
+    kind ("ConsoleApp")
+    language ("C++")
+    cppdialect ("C++latest")
+    staticruntime ("on")
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    pchheader ("mcpch.h")
+    pchsource ("%{prj.name}/src/mcpch.cpp")
+    
+    files ({
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/src/**.tpp",
+        "%{prj.name}/out/**",
+        "Premake5.lua"
+    })
+    
+    defines ({
+		"_CRT_SECURE_NO_WARNINGS"
+	})
+
+    includedirs ({ 
+        "%{prj.name}/src" -- project
+    })
+
+    debugdir ("run")
+    
+    filter ("system:windows")
+		systemversion ("latest")
+		
+	filter ("configurations:Debug")
+		runtime ("Debug")
+		symbols ("On")
+        defines ({
+            "NDEBUG"
+        })
+
+	filter ("configurations:Release")
+		runtime ("Release")
+		optimize ("on")
+
+    filter ("configurations:Dist")
+        runtime ("Release")
+        optimize ("on")
+        defines ({
+            "APP_DISTRIBUTION"
+        })
