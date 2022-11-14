19982f1a06bf1b4fbdaaf6e1aadd5da7fa90cc0b
twarug <patryk280502@gmail.com>
2022-11-14 23:24:38 +0100

    Cleanup PCH

--- MineClone/src/mcpch.h
+++ MineClone/src/mcpch.h
@@ -1,19 +1,28 @@
 #pragma once
 
-#include <iostream>
+// Logging / files
 #include <format>
-#include <map>
-#include <unordered_map>
 #include <fstream>
+#include <iostream>
 #include <filesystem>
+
+// Strings
 #include <string>
 #include <string_view>
+
+// Containers
+#include <set>
+#include <map>
 #include <span>
+#include <array>
 #include <vector>
+#include <unordered_map>
+
+// Others
+#include <memory>
+#include <chrono>
 #include <algorithm>
 #include <functional>
-#include <chrono>
-#include <array>
 
 #include "MineClone/Core.h"
 #include "MineClone/Math/Math.h"
