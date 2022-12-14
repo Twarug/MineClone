45b7600fa86d0425632884f55893ed28f08ec13a
Twarug <patryk280502@gmail.com>
2022-12-14 16:57:03 +0100

    Create Block base

---
+++ MineClone/src/MineClone/Game/Block/Block.cpp
@@ -0,0 +1,11 @@
+﻿#include "mcpch.h"
+#include "Block.h"
+
+namespace mc
+{
+    const Block& Block::AIR = REGISTRY.Register(new Block("air", float3{}, true));
+    const Block& Block::STONE = REGISTRY.Register(new Block("stone", float3(.5f)));
+
+    constexpr Block::Block(const std::string& name, float3 color, bool transparent)
+        : RegistryEntry(name), m_transparent(transparent), m_color(color) {}
+}
---
+++ MineClone/src/MineClone/Game/Block/Block.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "MineClone/Game/Registry/Registry.h"
+#include "MineClone/Game/Registry/RegistryEntry.h"
+
+namespace mc
+{
+    class Block : public RegistryEntry<Block>
+    {
+    public:
+        // ReSharper disable once CppInconsistentNaming
+        inline static Registry<Block> REGISTRY{};
+
+        static const Block& AIR;
+        static const Block& STONE;
+
+    protected:
+        explicit constexpr Block(const std::string& name, float3 color, bool transparent = false);
+
+    public:
+        bool IsTransparent() const { return m_transparent; }
+        float3 GetColor() const { return m_color; }
+
+    private:
+        bool m_transparent;
+        float3 m_color;
+    };
+}
